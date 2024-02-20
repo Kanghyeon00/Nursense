9c2ca8544769a2cc29dfea937d8c0ba40e72fa1c
Kanghyeon00 <hyeony0419@naver.com>
2024-02-20 13:10:00 +0900

    test

--- src/page/About.js
+++ src/page/About.js
@@ -14,11 +14,7 @@ const About = () => {
         <div className='aboutContainer'>
           <Header />
             <div className='aboutWrapper'>
-              <div className='aboutTextWrapper'>
-              <h1>Nursense</h1>
-              <p>다양한 간호술기 공부를 메타버스로 쉽고 재미있게</p>
-              <button className='aboutDownload' onClick={openAbout}>✔ 소개서 다운로드</button>
-              </div>
+              
             </div>
             <Footer />
         </div>
