00b913c28296904dd62fb2bfadc5b238150bcc3c
Kanghyeon00 <hyeony0419@naver.com>
2024-04-15 17:02:38 +0900

    test

--- src/components/BannerSlide.js
+++ src/components/BannerSlide.js
@@ -4,9 +4,15 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useLanguage } from "../LanguageContext";
+import { useNavigate } from 'react-router-dom';
 
 const Slide = () => {
   const { selectedLanguage, changeLanguage } = useLanguage();
+  const navigate = useNavigate();
+
+  const goToAbout = () => {
+    navigate('/about');
+  }
 
   const settings = {
     dots: true,
